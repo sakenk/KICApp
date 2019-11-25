d1ff431728f89472657da7e89e6f21d129b2104e
sakenk <s_kungozhin@hotmail.com>
2019-11-25 23:00:43 +0600

    End of section 3

---
+++ KICApp.API/Dtos/UserForLoginDto.cs
@@ -0,0 +1,11 @@
+namespace KICApp.API.Dtos
+{
+    public class UserForLoginDto
+    {
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+
+        
+    }
+}
