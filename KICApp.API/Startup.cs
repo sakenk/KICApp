be18024eeff73d0d247bd3534f84f33a28178984
sakenk <s_kungozhin@hotmail.com>
2020-03-29 21:22:14 +0600

    -end of section 3 update

--- KICApp.API/Startup.cs
+++ KICApp.API/Startup.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using KICApp.API.Data;
-// using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -16,8 +16,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
-// using Microsoft.IdentityModel.Tokens;
-
+using Microsoft.IdentityModel.Tokens;
 
 namespace KICApp.API
 {
@@ -39,17 +38,17 @@ namespace KICApp.API
             // dotnet 2.2 implementation commented
             // services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
             // services.AddCors();
-            services.AddScoped<IAuthRepository, AuthRepository>();            
-            // services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-            //     .AddJwtBearer(options => {
-            //         options.TokenValidationParameters = new TokenValidationParameters {
-            //             ValidateIssuerSigningKey = true,
-            //             IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII
-            //                 .GetBytes(Configuration.GetSection("AppSettings:Token").Value)),
-            //                 ValidateIssuer = false,
-            //                 ValidateAudience = false
-            //         };
-            //     });
+            services.AddScoped<IAuthRepository, AuthRepository>();        
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options => {
+                    options.TokenValidationParameters = new TokenValidationParameters {
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII
+                            .GetBytes(Configuration.GetSection("AppSettings:Token").Value)),
+                            ValidateIssuer = false,
+                            ValidateAudience = false
+                    };
+                });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -69,6 +68,9 @@ namespace KICApp.API
 
             app.UseRouting();
 
+            app.UseAuthentication();
+            app.UseAuthorization();
+            
             app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
 
             app.UseAuthorization();
